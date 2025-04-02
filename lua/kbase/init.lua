fd4a2633e3f2c87086c4228c81b7b5745102e7aa
Jack Anderson <jack.anderson@bjss.com>
2025-04-02 14:55:52 +0100

    initial tagging

--- lua/kbase/init.lua
+++ lua/kbase/init.lua
@@ -10,18 +10,6 @@ M.setup = function()
     print("Setup called")
 end
 
-
--- [bufnum, lnum, col, off]
--- Not associative array but is indexed. Remember lua is 1 based indexing
--- "bufnum" is zero, unless a mark like '0 or 'A is used, then it
--- is the buffer number of the mark.
--- "lnum" and "col" are the position in the buffer.  The first
--- column is 1.
--- The "off" number is zero, unless 'virtualedit' is used.  Then
--- it is the offset in screen columns from the start of the
--- character.  E.g., a position within a <Tab> or after the last
--- character.
-
 vim.keymap.set("v", "<leader>k", function()
     local cursor = vim.fn.getpos(".")
     local visualEnd= vim.fn.getpos("v")
@@ -33,7 +21,6 @@ vim.keymap.set("v", "<leader>k", function()
     -- It seems like the two positions are equal when we are just visually selecting a line
 
     local cursorLine, cursorCol, visualLine, visualCol = cursor[2], cursor[3], visualEnd[2], visualEnd[3]
-    print(cursorLine, cursorCol, visualLine, visualCol)
     local selectionStartLine, selectionEndLine, selectionStartCol, selectionEndCol =
         cursorLine, visualLine, cursorCol, visualCol
     if  cursorLine == visualLine and cursorCol == visualCol then
@@ -53,17 +40,18 @@ vim.keymap.set("v", "<leader>k", function()
     -- Visual line mode, we simply want to select the lines
     if mode == "v" then
         -- visual mode
-        selection = vim.api.nvim_buf_get_text(0, selectionStartLine -1, selectionStartCol, selectionEndLine -1, selectionEndCol, false) 
+        selection = vim.api.nvim_buf_get_text(0, selectionStartLine -1, selectionStartCol, selectionEndLine -1, selectionEndCol, {})
     elseif mode == "V" then
         -- visual line mode
         selection = vim.api.nvim_buf_get_lines(0, selectionStartLine -1, selectionEndLine, false) 
     end
-    P(selection)
+    local selectedLines = table.concat(selection, "\n")
 
-
-    -- local selected = vim.api.nvim_buf_get_text(0, selectionStartLine-1, selectionStartCol-1, selectionEndLine-1, selectionEndCol-1, {})
-    -- print(selectionStartLine, selectionStartCol, selectionEndLine, selectionEndCol)
-    -- P(selected)
-
+    print(selectedLines)
+    local fileType = vim.filetype
+    local tag = vim.fn.input("%s tag:", fileType)
+    -- Go back to normal mode, replacing termcodes like <C-c> with their internal representation rather than 
+    -- feeding these exact keys
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-c>', true, false, true), 'n', true)
 end)
 return M
